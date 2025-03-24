cf31cd07e568719b5d63e1c496421dfa30243743
Musa Adanur <musa-adanur@hotmail.com>
2025-03-24 03:13:49 +0300

    Add interactive Rive animation

--- App.tsx
+++ App.tsx
@@ -1,130 +1,86 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import React, { useState } from 'react';
+import Rive, { RiveRef } from 'rive-react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [count, setCount] = useState(0);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const riveRef = React.useRef<RiveRef>(null);
+  const handlePlay = () => {
+
+    riveRef.current?.fireState('State Machine 1', 'Trigger 1')
+    setCount(p => p + 1)
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
+    <View style={styles.container}>
+      <View style={styles.sectionOne}>
+        <Text style={styles.count}>{count}</Text>
+      </View>
+      <View style={styles.sectionTwo}>
+        <View style={styles.sectionTwoOne}>
+          <Rive ref={riveRef} autoplay={false}
+            resourceName='twerkDuo'
+            artboardName="duoTwerk"
+            stateMachineName="State Machine 1"
+          />
         </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+
+      </View>
+      <View style={styles.sectionThree}>
+        <TouchableOpacity style={styles.button} onPress={handlePlay}>
+          <Text style={styles.buttonText}>Saydır</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
   },
-  sectionTitle: {
+  sectionOne: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  count: {
+    fontSize: 72,
+    fontFamily: 'Concert One',
+    color: '##3c3941'
+  },
+  sectionTwo: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+  },
+  sectionTwoOne: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    padding: 35,
+  },
+  sectionThree: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#3c3941',
+    padding: 20,
+    marginHorizontal: 10,
+    borderRadius: 16,
+    width: '40%',
+  },
+  buttonText: {
+    textAlign: 'center',
     fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    color: 'white',
+    fontFamily: 'Concert One',
   },
 });
 
