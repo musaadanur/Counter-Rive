f189924b2b9b95e68daa04101b7520f2e22f5596
Musa Adanur <musa-adanur@hotmail.com>
2025-03-26 03:31:11 +0300

    added new rive animation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Rive, { RiveRef } from 'rive-react-native'
+import React, { useEffect, useState } from 'react';
+import Rive, { RiveRef } from 'rive-react-native';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 function App(): React.JSX.Element {
@@ -7,11 +7,18 @@ function App(): React.JSX.Element {
 
   const riveRef = React.useRef<RiveRef>(null);
   const handlePlay = () => {
-
-    riveRef.current?.fireState('State Machine 1', 'Trigger 1')
-    setCount(p => p + 1)
+    riveRef.current?.fireState('State Machine 1', 'Trigger 1');
+    setCount((p) => p + 2);
   };
-
+  const handlePlayL = () => {
+    riveRef.current?.fireState('State Machine 1', 'Trigger L');
+    setCount((p) => p + 1);
+  };
+  const handlePlayR = () => {
+    riveRef.current?.fireState('State Machine 1', 'Trigger R');
+    setCount((p) => p + 1);
+  };
+  
 
   return (
     <View style={styles.container}>
@@ -20,17 +27,30 @@ function App(): React.JSX.Element {
       </View>
       <View style={styles.sectionTwo}>
         <View style={styles.sectionTwoOne}>
-          <Rive ref={riveRef} autoplay={false}
-            resourceName='twerkDuo'
-            artboardName="duoTwerk"
-            stateMachineName="State Machine 1"
+          <Rive
+            ref={riveRef}
+            autoplay={true}
+            resourceName='duotwerk_v05'
+            artboardName='duoTwerk'
+            stateMachineName='State Machine 1'
           />
         </View>
-
       </View>
       <View style={styles.sectionThree}>
+        <TouchableOpacity style={styles.smallButton} onPress={handlePlayL}>
+          <View style={styles.buttonContainer}>
+            <Text style={[styles.buttonText, { color: '#959ea0' }]}>L</Text>
+          </View>
+        </TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={handlePlay}>
-          <Text style={styles.buttonText}>Saydır</Text>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>Saydır</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.smallButton} onPress={handlePlayR}>
+          <View style={styles.buttonContainer}>
+            <Text style={[styles.buttonText, { color: '#959ea0' }]}>R</Text>
+          </View>
         </TouchableOpacity>
       </View>
     </View>
@@ -44,41 +64,49 @@ const styles = StyleSheet.create({
   },
   sectionOne: {
     flex: 1,
-    backgroundColor: '#ffffff',
     justifyContent: 'center',
     alignItems: 'center',
   },
   count: {
     fontSize: 72,
     fontFamily: 'Concert One',
-    color: '##3c3941'
+    color: '#3c3941',
   },
   sectionTwo: {
-    backgroundColor: '#ffffff',
     flex: 1,
   },
   sectionTwoOne: {
-    backgroundColor: '#ffffff',
     flex: 1,
     padding: 35,
   },
   sectionThree: {
-    backgroundColor: '#ffffff',
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+    gap: 8,
   },
   button: {
+    height: 61,
     backgroundColor: '#3c3941',
-    padding: 20,
-    marginHorizontal: 10,
-    borderRadius: 16,
+    borderRadius: 100,
     width: '40%',
+  },
+  smallButton: {
+    backgroundColor: '#e5e9e9',
+    borderRadius: 24,
+    width: 61,
+    height: 61,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonText: {
     textAlign: 'center',
-    fontSize: 24,
+    textAlignVertical: 'center',
+    fontSize: 20,
     color: 'white',
     fontFamily: 'Concert One',
   },
